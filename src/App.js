264504dd87233fd6c142e037046c5ae9d72c3779
Marvin Kaaya <marv@192.168.0.102>
2022-05-07 12:06:10 +0300

    new code

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
-import React from 'react';
-import { StreamChat } from 'stream-chat';
-import { Chat, Channel, ChannelHeader, MessageInput, MessageInputSmall, VirtualizedMessageList, Window } from 'stream-chat-react';
+import React from "react";
+import { ChatEngine } from "react-chat-engine";
+import { ChatFeed } from "./components/ChatFeed";
 
-import 'stream-chat-react/dist/css/index.css';
+import "./App.css";
 
-const chatClient = StreamChat.getInstance('dz5f4d5kzrue');
-const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoic3ByaW5nLWxlYWYtNyIsImV4cCI6MTY1MTkyNTM2M30.GAJXg-2PlvUjVYIlhi4hRNGrebDycU67vFRJTVa5BkE';
-
-chatClient.connectUser(
-  {
-    id: 'spring-leaf-7',
-    name: 'spring',
-    image: 'https://getstream.io/random_png/?id=spring-leaf-7&name=spring',
-  },
-  userToken,
-);
-
-const channel = chatClient.channel('livestream', 'spacex', {
-  image: 'https://goo.gl/Zefkbx',
-  name: 'SpaceX launch discussion',
-});
-
-const App = () => (
-  <Chat client={chatClient} theme='livestream dark'>
-    <Channel channel={channel}>
-      <Window>
-        <ChannelHeader live />
-        <VirtualizedMessageList />
-        <MessageInput Input={MessageInputSmall} focus />
-      </Window>
-    </Channel>
-  </Chat>
-);
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="a29a7365-986c-4541-a037-b7eca39e4141"
+      userName="marv"
+      userSecret="qwerty"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} /> }
+    />
+  );
+};
 
 export default App;
