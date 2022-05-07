e19c24b5f74d0227f169342f5c949b2f0a31ecee
Marvin Kaaya <marv@192.168.0.102>
2022-05-07 12:16:11 +0300

    additions

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React from "react";
-import { ChatEngine } from "react-chat-engine";
-import { ChatFeed } from "./components/ChatFeed";
+import { ChatEngine } from 'react-chat-engine';
 
-import "./App.css";
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
 
 const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
     <ChatEngine
       height="100vh"
-      projectID="a29a7365-986c-4541-a037-b7eca39e4141"
-      userName="marv"
-      userSecret="qwerty"
-      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} /> }
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
   );
 };
+
+// infinite scroll, logout, more customizations...
 
 export default App;
